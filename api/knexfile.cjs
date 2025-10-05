80baf7d13060fbd556ec683a0a528cef512a3832
Jeremy0404 <jeremy.collombclerc@gmail.com>
2025-10-05 11:45:54 +0200

    fix: migration

--- api/knexfile.cjs
+++ api/knexfile.cjs
@@ -1,14 +1,11 @@
-// api/knexfile.cjs
 require("dotenv").config();
 
 /** @type {import('knex').Knex.Config} */
 module.exports = {
   client: "pg",
   connection: process.env.DATABASE_URL,
-  pool: { min: 0, max: 10 },
   migrations: {
-    directory: "./migrations",
-    extension: "ts",
-    tableName: "knex_migrations",
+    directory: "./dist/migrations",
+    extension: "js",
   },
 };
