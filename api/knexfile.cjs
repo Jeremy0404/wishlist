d92efbe335f20b815e3a4002d2369607bd6313b5
Jeremy0404 <jeremy.collombclerc@gmail.com>
2025-10-05 13:00:08 +0200

    fix: build

--- api/knexfile.cjs
+++ api/knexfile.cjs
@@ -5,7 +5,10 @@ module.exports = {
   client: "pg",
   connection: process.env.DATABASE_URL,
   migrations: {
-    directory: "./dist/migrations",
-    extension: "js",
+    directory:
+      process.env.NODE_ENV === "production"
+        ? "./dist/migrations"
+        : "./migrations",
+    extension: process.env.NODE_ENV === "production" ? "js" : "ts",
   },
 };
